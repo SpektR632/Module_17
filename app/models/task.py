73743b529733947b55a5c12d5363bc7dacf6aff1
SpektR632 <spektr.632@yandex.ru>
2024-10-31 06:23:40 +0300

    Задача "Модели SQLAlchemy"

--- app/models/task.py
+++ app/models/task.py
@@ -13,7 +13,8 @@ class Task(Base):
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True)
     slug = Column(String, unique=True, index=True)
-    tasks = relationship('User', back_populates='tasks')
+    user = relationship('User', back_populates='tasks')
 
+print(CreateTable(Task.__table__))
--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from app.backend.db import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-
+from sqlalchemy.schema import CreateTable
 
 class User(Base):
     __tablename__ = 'users'
@@ -11,11 +11,11 @@ class User(Base):
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String, unique=True, index=True)
-    tasks = relationship('Task', back_populates='tasks')
+    tasks = relationship('Task', back_populates='user')
 
 
 
-
+print(CreateTable(User.__table__))
 
 
 
